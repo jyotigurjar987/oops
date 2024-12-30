30b85fbdc01c1fc630c614545ae0acfe8dacd56d
jyotigurjar987 <jyansh987@gmail.com>
2024-12-30 22:04:08 +0530

    Typecasting file added

---
+++ AutomaticTypeCasting.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class AutomaticTypeCasting {
+	
+	public static void main(String[] args) {
+		
+		int num = 5;
+		double result = num;
+		System.out.println("The result is:"+result);
+	}
+
+}
